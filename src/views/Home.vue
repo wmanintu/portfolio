374fbca87e8aeb1e350c86c14d91dc12fcb11502
Wut Manintu <manintu.wut@gmail.com>
2023-07-22 02:46:12 -0400

    clean up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,66 @@
 <template lang="html">
-  <div class="home-container">
-    <div class="columns">
-      <div class="column is-12-mobile is-3-tablet is-4-desktop my-avatar-container">
-        <img src="https://i.ibb.co/Cb6grB5/IMG-4494-3.jpg" alt="IMG-4494-3" border="0" class="my-face"/>
-      </div>
-      <div class="column is-12-mobile is-9-tablet is-8-desktop">
-        <div class="green-text">
-          Hello! my name is Wut.<br>I am a Full Stack Web Application Developer.
-        </div>
-        <i class="fab fa-github" @click="openUrl('https://github.com/wmanintu')"></i>
-        <i class="fas fa-envelope" @click="mailTo('mailto:manintu.wut@gmail.com')"></i>
-      </div>
-    </div>
-  </div>
+	<div class="home-container">
+		<div class="columns">
+			<div
+				class="column is-12-mobile is-3-tablet is-4-desktop my-avatar-container"
+			>
+				<img
+					src="https://i.ibb.co/Cb6grB5/IMG-4494-3.jpg"
+					alt="IMG-4494-3"
+					border="0"
+					class="my-face"
+				/>
+			</div>
+			<div class="column is-12-mobile is-9-tablet is-8-desktop">
+				<div class="green-text">
+					Hello! my name is Wut.<br />I am a Front-End Developer.
+				</div>
+				<i
+					class="fab fa-github"
+					@click="openUrl('https://github.com/wmanintu')"
+				></i>
+				<i
+					class="fas fa-envelope"
+					@click="mailTo('mailto:manintu.wut@gmail.com')"
+				></i>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  methods: {
-    mailTo (email) {
-      window.location.href = email
-    },
-    openUrl (url)  {
-      window.open(url,'_blank')
-    }
-  }
+	methods: {
+		mailTo(email) {
+			window.location.href = email
+		},
+		openUrl(url) {
+			window.open(url, "_blank")
+		},
+	},
 }
 </script>
 
 <style lang="css" scoped>
 .my-face {
-  border-radius: 400px;
-  width: 200px;
+	border-radius: 400px;
+	width: 200px;
 }
 .my-avatar-container {
-  text-align: center;
+	text-align: center;
 }
-.fab, .fas {
-  margin: 15px;
-  font-size: 30px;
+.fab,
+.fas {
+	margin: 15px;
+	font-size: 30px;
 }
-.fab:hover, .fas:hover {
-  cursor: pointer;
+.fab:hover,
+.fas:hover {
+	cursor: pointer;
 }
 @media (min-width: 740px) {
-  .home-container {
-    margin-top: 200px;
-  }
+	.home-container {
+		margin-top: 200px;
+	}
 }
 </style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,67 +1,43 @@
 <template lang="html">
-  <div class="tile is-ancestor">
-    <div class="tile is-vertical is-6">
-      <div class="tile">
-        <div class="tile is-parent is-vertical">
-          <article class="tile is-child notification is-primary">
-            <PreOrderArticle/>
-          </article>
-        </div>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-danger">
-          <YouTubeQueueArticle/>
-        </article>
-      </div>
-    </div>
-    <div class="tile is-vertical is-6">
-      <div class="tile">
-        <div class="tile is-parent is-vertical">
-          <article class="tile is-child notification is-success">
-            <PhotoGalleryArticle />
-          </article>
-        </div>
-      </div>
-      <!-- <div class="tile is-parent">
-        <article class="tile is-child notification is-danger">
-          <YouTubeQueueArticle/>
-        </article>
-      </div>
-      <div class="tile">
-        <div class="tile is-parent is-vertical">
-          <article class="tile is-child notification is-success">
-            <div class="content">
-              <p class="title">Spotify API</p>
-              <p class="subtitle">my person favorites</p>
-              <div class="content">
-              </div>
-            </div>
-          </article>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-danger">
-            <YouTubeQueueArticle/>
-          </article>
-        </div>
-      </div> -->
-    </div>
-  </div>
-
+	<div class="tile is-ancestor">
+		<div class="tile is-vertical is-6">
+			<div class="tile">
+				<div class="tile is-parent is-vertical">
+					<article class="tile is-child notification is-primary">
+						<PreOrderArticle />
+					</article>
+				</div>
+			</div>
+			<div class="tile is-parent">
+				<article class="tile is-child notification is-danger">
+					<YouTubeQueueArticle />
+				</article>
+			</div>
+		</div>
+		<div class="tile is-vertical is-6">
+			<div class="tile">
+				<div class="tile is-parent is-vertical">
+					<article class="tile is-child notification is-success">
+						<PhotoGalleryArticle />
+					</article>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import PreOrderArticle from '../components/PreOrderArticle'
-import YouTubeQueueArticle from '../components/YouTubeQueueArticle'
-import PhotoGalleryArticle from '../components/PhotoGalleryArticle'
+import PreOrderArticle from "../components/PreOrderArticle"
+import YouTubeQueueArticle from "../components/YouTubeQueueArticle"
+import PhotoGalleryArticle from "../components/PhotoGalleryArticle"
 
 export default {
-  components: {
-    PreOrderArticle,
-    YouTubeQueueArticle,
-    PhotoGalleryArticle
-  }
+	components: {
+		PreOrderArticle,
+		YouTubeQueueArticle,
+		PhotoGalleryArticle,
+	},
 }
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
