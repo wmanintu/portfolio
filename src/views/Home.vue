fe88e2bfe62e8a6426d5dd9669573bc240d59727
Efge <manintu.wut@gmail.com>
2023-07-26 07:42:20 -0400

    add css grid reponsiveness

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,65 @@
 <template>
-	<div>
-		<div class="columns">
-			<div class="column"></div>
-			<div class="column is-four-fifths-touch is-three-fifths-desktop">
-				<div class="wrapper">
-					<div class="item1">
-						<ProfileCard />
-					</div>
-					<div class="item2"><DarkModeCard /></div>
-					<div class="item3 ></div> <div class=" item4></div>
-				</div>
-			</div>
-			<div class="column"></div>
-		</div>
-	</div>
+  <div>
+    <div class="columns">
+      <div class="column"></div>
+      <div class="column is-three-fifths-desktop is-10-tablet is-full-mobile">
+        <div class="wrapper">
+          <div class="item1">
+            <ProfileCard />
+          </div>
+          <div class="item2"><DarkModeCard /></div>
+          <div class="item3"></div>
+          <div class="item4"></div>
+        </div>
+      </div>
+      <div class="column"></div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import DarkModeCard from "../components/DarkModeCard.vue"
-import ProfileCard from "../components/ProfileCard.vue"
+import DarkModeCard from "../components/DarkModeCard.vue";
+import ProfileCard from "../components/ProfileCard.vue";
 
 function openUrl(url) {
-	window.open("https://github.com/wmanintu", "_blank")
+  window.open("https://github.com/wmanintu", "_blank");
 }
 </script>
 
 <style scoped>
-.green {
-	background-color: green;
+/* desktop */
+@media screen and (min-width: 701px) {
+  .wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    justify-items: center;
+    gap: 1rem;
+    grid-template-areas:
+      "a a b ."
+      "a a . .";
+  }
 }
-.wrapper {
-	display: grid;
-	grid-template-columns: auto;
-	grid-template-rows: repeat(6, auto);
-	/* height: 100%; */
-	/* width: 100%; */
-	gap: 12px;
-	grid-template-areas:
-		"a a b c"
-		"a a d e"
-		"f f f f";
+/* mobile */
+@media screen and (max-width: 700px) {
+  .wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    justify-items: center;
+    gap: 1rem;
+    grid-template-areas:
+      "a a"
+      "b .";
+  }
 }
 .item1 {
-	grid-area: a;
+  grid-area: a;
 }
 .item2 {
-	grid-area: b;
+  grid-area: b;
 }
 .item3 {
-	grid-area: c;
-}
-.item4 {
-	grid-area: d;
+  grid-area: c;
 }
 </style>
